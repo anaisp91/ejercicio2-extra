a43e868dc2ceee16b40c1e5442433c57caa5ad7e
Anais <anais.planas.91@gmail.com>
2025-12-30 20:35:42 +0100

    inicio JS

--- js/index.js
+++ js/index.js
@@ -27,8 +27,66 @@ const fabricaArticulos = new FabricaArticulos()
 
 const listaArticulos = []
 
+//addArticulo(nombre)
+
+function addArticulo(nombre){
+    const articulo = fabricaArticulos.createArticulo(nombre)
+    listaArticulos.push(articulo)
+    return articulo
+}
+
+//removeArticulo(id)
+
+function removeArticulo(id){
+    const index = listaArticulos.findIndex(articulo => articulo.id === id)
+    if(index !== -1){
+        listaArticulos.splice(index, 1)
+    }
+}
+
+// toggleComprado(id)
+
+function toggleComprado(id){
+    const articulo = listaArticulos.find(articulo => articulo.id === id)
+    if(articulo){
+        articulo.comprado = !articulo.comprado
+    }
+}
+
+// clearLista()
+
+function clearLista(){
+    listaArticulos.length = 0
+}
+
 //Elementos del DOM
-// Inicializacion
-//Eventos
-//Logica de Negocio
+
+const formulario = document.getElementById('formulario')
+const inputArticulo = document.getElementById('articulo')
+const botonAnadir = document.getElementById('botonAnadir')
+const botonNuevaLista = document.getElementById('nuevaLista')
+const listaDesordenada = document.getElementById('listaCompra')
+
+
 //Renderizado
+
+function renderLista(){
+
+    listaDesordenada.innerHTML = ''
+    
+    listaArticulos.forEach(articulo =>{
+        const li = document.createElement('li')
+        li.textContent = articulo.nombre
+        li.dataset.id = articulo.id
+        listaDesordenada.appendChild(li)
+
+        const botonBorrar = document.createElement('button')
+        botonBorrar.textContent = 'Borrar'
+        li.appendChild(botonBorrar)
+
+        const checkBox = document.createElement('input')
+        checkBox.type = 'checkbox'
+        li.appendChild(checkBox)
+
+    })
+}
